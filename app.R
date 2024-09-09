cf675b932aedb09499bdd45f46633a602a36d858
Daniel Kunk <85962928+dankunk@users.noreply.github.com>
2024-09-08 18:17:50 -0600

    app.R
    Reverting back... Working App. Verified with 2023 EPG data.

--- app.R
+++ app.R
@@ -1,7 +1,16 @@
 rm(list=ls())
 
-lapply(c('discoEPG', 'shiny', 'shinyWidgets', 'dplyr', 'rhandsontable', 'gt',
-         'svglite', 'DiagrammeR', 'DiagrammeRsvg', 'ggplot2'), library, character.only = TRUE)
+require(shiny)
+require(shinyWidgets)
+require(rhandsontable)
+require(gt)
+require(ggplot2)
+require(DiagrammeR)
+require(DiagrammeRsvg)
+require(svglite)
+require(tidyverse)
+require(discoEPG)
+require(bslib)
 
 # Code for running on Clay's laptop
 # appPath <- "/Users/c/Documents/EPG Project/EPGShinyApp/Deployed/"
@@ -39,11 +48,11 @@ waveform_labels = c("np" = 1,
 
 # ---- Shiny App Starts Here ----
 shinyApp(
-  
+
   ui <- fluidPage(
-    
+
     tabsetPanel(id = "tabsets",
-                
+
                 # ---- Tab 0: Home Page - Background & Instructions
                 tabPanel(title = 'Home Page', fluid = TRUE, value = 'homeTab',
                          #titlePanel("EPG Shiny App: Start Here"),
@@ -63,7 +72,7 @@ shinyApp(
                            ),
                            p(style = "text-align: justify; font-size = 50px",
                              strong("We value your feedback!"), "If you encounter any issues, find bugs, or have suggestions for improvement, please don’t hesitate to reach out to", strong("vamsi.nalam@colostate.edu"), "and let us know."
-                           ),
+                             ),
                            hr(),
                            # project description essay
                            h4(strong("Project Description")),
@@ -99,11 +108,11 @@ shinyApp(
                            br(),
                            p(style="text-align: justify; font-size = 50px",
                              "You're all done with data processing! Now you can move on to other tabs to see data validation errors, summary statistics, and plots."
-                             # fin tab 0, begin functional tabs
+                          # fin tab 0, begin functional tabs
                            )
                          )
                 ),
-                
+
                 # ---- Tab 1: Upload Data & Calculate Parameters
                 tabPanel(title = "Upload Data", fluid = TRUE, value = "dataTab",
                          titlePanel("EPG Shiny App: Upload Data and Calculate Parameters"),
@@ -115,7 +124,7 @@ shinyApp(
                                             choices = c('Files/Treatment Assignments' = 'files',
                                                         'Raw Data' = 'rawData',
                                                         'Computed Parameters' = 'paramData')
-                                            
+
                                )
                              }),
                              conditionalPanel("output.filesUploaded", {
@@ -141,13 +150,13 @@ shinyApp(
                            )
                          )
                 ),
-                
+
                 # ---- Tab 1b: Validation Errors
                 tabPanel(title = "Validation Errors", fluid = TRUE, value = "validationErrors",
                          titlePanel("EPG Shiny App: Raw Data Validation Errors"),
                          verbatimTextOutput("validationText")
                 ),
-                
+
                 # ---- Tab 2: Summary Statistics
                 tabPanel(title = "Summary Statistics", fluid = TRUE, value = "summaryStatsTab",
                          titlePanel("EPG Shiny App: Summary Statistics, by Treatment"),
@@ -189,8 +198,8 @@ shinyApp(
                            )
                          )
                 ),
-                
-                
+
+
                 # ---- Tab 3: Boxplots
                 tabPanel(title = "Boxplots", fluid = TRUE, value = "boxplotsTab",
                          titlePanel("EPG Shiny App: Boxplots, by Treatment"),
@@ -220,8 +229,8 @@ shinyApp(
                            )
                          )
                 ),
-                
-                
+
+
                 # ---- Tab 4: TimeSeries Plots
                 tabPanel(title = "Timeseries Plot", fluid = TRUE, value = "timeseriesTab",
                          titlePanel("EPG Shiny App: Timeseries plots, by Treatment"),
@@ -250,8 +259,8 @@ shinyApp(
                            )
                          )
                 ),
-                
-                
+
+
                 # ---- Tab 5: Kinetogram
                 tabPanel(title = "Kinetogram", fluid = TRUE, value = "kinetogramTab",
                          titlePanel("EPG Shiny App: Kinetograms, by Treatment"),
@@ -275,7 +284,7 @@ shinyApp(
                              conditionalPanel("output.kinetogramCalculated", {
                                downloadButton(outputId = "downloadKinetogramPlot2", label = "Download Kinetogram Plot, 2nd Treatment")
                              }),
-                             
+
                            ),
                            mainPanel(
                              p(style="text-align: justify; font-size = 50px",
@@ -290,16 +299,16 @@ shinyApp(
                            )
                          )
                 )
-                
+
     )
-    
+
   ),
-  
-  
+
+
   server <- function(input, output, session) {
-    
+
     ### ----- Define Control Variables ----
-    
+
     output$filesUploaded <- reactive(if(length(input$files) > 0) TRUE else FALSE)
     output$trtAssignParamCalc <- reactive(if(input$assignTrtCalcParam > 0) TRUE else FALSE)
     output$sumstatsCalculated <- reactive(if(input$calculateSummaryStats > 0) TRUE else FALSE)
@@ -310,19 +319,19 @@ shinyApp(
     outputOptions(output, "sumstatsCalculated", suspendWhenHidden = FALSE)
     outputOptions(output, "timeseriesCalculated", suspendWhenHidden = FALSE)
     outputOptions(output, "kinetogramCalculated", suspendWhenHidden = FALSE)
-    
-    
+
+
     ### ----- Tab 0: Home Page ----
-    
+
     # Render images
     output$discoLogo <- renderImage(list(src = paste0(appPath, 'discoEPGLogo.png'), contentType = 'image/png', width = 500, height = 100), deleteFile = F)
     output$uploadDataScreenshot <- renderImage(list(src = paste0(appPath, 'uploadDataScreenshot.png'), contentType = 'image/png', width = 1500, height = 500), deleteFile = F)
     output$assignTreatmentsScreenshot <- renderImage(list(src = paste0(appPath, 'assignTreatmentsScreenshot.png'), contentType = 'image/png', width = 1000, height = 400), deleteFile = F)
     output$afterAssignTreatmentsScreenshot <- renderImage(list(src = paste0(appPath, 'afterAssignTreatmentsScreenshot.png'), contentType = 'image/png', width = 1500, height = 400), deleteFile = F)
-    
-    
+
+
     ### ----- Tab 1: Data Processing ----
-    
+
     # Table for Assigning Treatments
     metadata_handson <- reactive({
       filenames <- sort(input$files$name)
@@ -330,63 +339,63 @@ shinyApp(
       rhandsontable(df) %>%
         hot_table(highlightCol = TRUE, highlightRow = TRUE)
     })
-    
+
     output$metadata_hot <- renderRHandsontable(metadata_handson())
-    
-    
+
+
     dataAndErrorsList <- reactive({
-      
+
       filepaths <- input$files$datapath
       filenames <- input$files$name
-      
+
       # Read in all .ANA data into a list of dataframes and error messages
       out <- combine_ana(filenames = filenames, directory = NULL, filepaths_shiny = filepaths, waveform_labels = waveform_labels)
-      
+
       nErrors <- length(out$errors)
       # If any errors in ANA files, show a pop-up warning to the user
       if(nErrors > 0) {
         showNotification(ui=paste0("** WARNING ** Data validation checks failed in ", nErrors, " files! See the 'Data Validation' page for details."),
                          type = 'warning', closeButton = TRUE, duration = 10)
       }
-      
+
       return(out)
     })
-    
-    
+
+
     # Assign treatments to all data and return a single data frame
     alldata <- reactive({
       dat <- dataAndErrorsList()$data
-      
+
       # Get updated treatment assignments and merge to data
       meta <- hot_to_r(input$metadata_hot)
       dat <- merge(dat, meta, by = 'filename')
       return(dat)
     })  %>% bindEvent(input$assignTrtCalcParam)
-    
+
     # Output data table
     output$data_gt <- render_gt(alldata())
-    
+
     # Calculate Parameters and return a data frame
     paramData <- reactive({
-      
+
       dat <- alldata()
       if(nrow(dat) > 0) {
         # Convert combined ANA data to parameters
         epgData <- ana_to_parameters(ana_df = dat, waveform_labels = waveform_labels)
-        
+
         # Check if any files have > 70% of total time spent in F, G or np. If so, remove these from analysis and show notification in app.
         idx <- which(epgData$acronym == "%timeinF+G+np" & epgData$value > 70)
-        
+
         if(length(idx) > 0) {
           filesToRemove <- unique(epgData$filename[idx])
           showNotification(ui=paste("These files have > 70% of time spent in F/G/np phases combined, and will be omitted from analyses: ", paste(filesToRemove, collapse = ', ')),
                            type = 'warning', closeButton = TRUE, duration = NULL)
           epgData <- subset(epgData, !(filename %in% filesToRemove))
         }
-        
+
         # Remove this variable, not used in analyses
         epgData <- subset(epgData, acronym != "%timeinF+G+np")
-        
+
         # Merge to treatments
         meta <- hot_to_r(input$metadata_hot)
         epgData <- merge(epgData, meta, by = c('filename'))
@@ -395,7 +404,7 @@ shinyApp(
         return(NULL)
       }
     }) %>% bindEvent(input$assignTrtCalcParam)
-    
+
     # Export Data
     output$downloadData <- downloadHandler(
       filename = function() {
@@ -405,17 +414,17 @@ shinyApp(
         write.csv(paramData(), file, row.names = FALSE)
       }
     )
-    
+
     # Format Parameter Table For Display
     output$params_gt <- render_gt({
       paramDat <- select(paramData(), -one_of('description', 'treatment'))
-      
+
       trtKey <- hot_to_r(input$metadata_hot)
       trtKey <- subset(trtKey, filename %in% unique(paramDat$filename))
-      
+
       # Need to group columns by treatment. and then sort alphabetically(?)
       trtKey <- trtKey[order(trtKey$treatment, trtKey$filename), ]
-      
+
       # Rearrange so each filename has its own column
       paramList <- split(paramDat, paramDat$filename)
       paramList <- lapply(paramList, function(df) {
@@ -428,23 +437,23 @@ shinyApp(
           paramDat <- merge(paramDat, paramList[[i]], by='acronym')
         }
       }
-      
+
       # Sort columns and add Treatment prefix to file columns
       paramDat <- relocate(paramDat, c('acronym', trtKey$filename))
       for(tx in unique(trtKey$treatment)) {
         idx <- colnames(paramDat) %in% trtKey$filename[trtKey$treatment == tx]
         colnames(paramDat)[idx] <- paste(tx, colnames(paramDat)[idx], sep = '^^^')
       }
-      
+
       paramTbl <- paramDat %>%
         gt() %>%
         cols_label(acronym = 'Parameter') %>%
         tab_spanner_delim(columns = -one_of('acronym'), delim = '^^^')
-      
+
       return(paramTbl)
-      
+
     }) %>% bindEvent(input$assignTrtCalcParam)
-    
+
     ### ----- Tab 1b: Validation Errors ----
     output$validationText <- renderText({
       errors <- dataAndErrorsList()$errors
@@ -458,10 +467,10 @@ shinyApp(
       }
       return(msg)
     })
-    
-    
+
+
     ### ----- Tab 2: Summary Statistics Tables ----
-    
+
     summaryStats <- reactive({
       compute_summary_stats(parameter_df = paramData(),
                             parameter_metadata = parameterMetadata,
@@ -471,39 +480,39 @@ shinyApp(
                             alpha = 0.05,
                             p_adjust_method = 'bonferroni')
     }) %>% bindEvent(input$calculateSummaryStats)
-    
+
     # Summary Stats Table (for Display)
     output$summaryStats_gt <- render_gt({
-      
+
       sumStats <- summaryStats()
       # Convert NA to empty cell
       dunnCols <- grep('dunn_pval', colnames(sumStats))
       for(dc in dunnCols) {
         sumStats[is.na(sumStats[ , dc]), dc] <- ''
       }
-      
+
       # Make pretty
       sumStats <- sumStats %>%
         select(-one_of('experiment')) %>%
         rename(Parameter = acronym, Unit = unit)
-      
+
       if("dunn_pval^^overall" %in% colnames(sumStats)) {
         sumStats <- arrange(sumStats, `dunn_pval^^overall`)
       }
-      
+
       colnames(sumStats) <- gsub("mean.se", "Mean \u00B1 SE", colnames(sumStats))
       colnames(sumStats) <- gsub("dunn_pval", "p-value", colnames(sumStats))
-      
+
       sumStats_gt <- sumStats %>%
         gt() %>%
         tab_header(title = "Summary Statistics, by Treatment") %>%
         tab_spanner_delim(delim = '^^')  %>%
         tab_stubhead(label = "Treatments")
-      
+
       return(sumStats_gt)
-      
+
     })
-    
+
     # Update treatments available in pick-list based on parameter group pick-list, and data
     observeEvent(input$tabsets, {
       # Run only if Summary Stats Tab/Page is selected, and treatments have been assigned
@@ -511,9 +520,9 @@ shinyApp(
         tx <- unique(hot_to_r(input$metadata_hot)$treatment)
         updatePickerInput(session = session, inputId = "treatmentPicker", choices = tx, selected = tx)
       }
-      
+
     }, ignoreInit = TRUE)
-    
+
     # Update parameters available in pick-list based on parameter group pick-list, and data
     observeEvent(input$parameterGroupPicker, {
       parameterGroups <- input$parameterGroupPicker
@@ -522,10 +531,10 @@ shinyApp(
         select('acronym') %>%
         distinct() %>%
         as.vector()
-      
+
       updatePickerInput(session = session, inputId = "parameterPicker", choices = params, selected = params)
     }, ignoreInit = TRUE)
-    
+
     # Export Summary Statistics (This works on a remotely served version)
     output$downloadSummaryStats <- downloadHandler(
       filename = function() {
@@ -535,9 +544,9 @@ shinyApp(
         write.csv(summaryStats(), file, row.names = FALSE)
       }
     )
-    
+
     ### ----- Tab 3: Box Plots ----
-    
+
     # Update treatments available in pick-list
     observeEvent(input$tabsets, {
       # Run only if Timeseries Tab/Page is selected, and treatments have been assigned
@@ -546,12 +555,12 @@ shinyApp(
         updatePickerInput(session = session, inputId = "treatmentPickerBoxplots", choices = tx, selected = tx)
       }
     }, ignoreInit = TRUE)
-    
+
     # Boxplots
     output$boxplots <- renderPlot({
       dat <- paramData()
       sumStats <- summaryStats()
-      
+
       if(input$boxplotRadio == 'Statistically significant') {
         pvalCols <- grep('dunn_pval', colnames(sumStats), value = TRUE)
         if(length(pvalCols) > 0) {
@@ -567,11 +576,11 @@ shinyApp(
           showNotification("Data only exists for 1 treatment. Cannot compute statistical differences.",
                            type = 'warning', closeButton = TRUE, duration = 5)
         }
-        
+
       } else {
         dat <- subset(dat, acronym %in% input$boxplotParameterPicker)
       }
-      
+
       if(nrow(dat) > 0) {
         subDat <- subset(dat, treatment %in% input$treatmentPickerBoxplots)
         out <- ggplot(subDat, aes(x=treatment, y=value, color=treatment)) +
@@ -584,11 +593,11 @@ shinyApp(
       }
       return(out)
     }) %>% bindEvent(input$showBoxplots)
-    
-    
-    
+
+
+
     ### ----- Tab 4: Timeseries Plot ----
-    
+
     # Update treatments available in pick-list
     observeEvent(input$tabsets, {
       # Run only if Timeseries Tab/Page is selected, and treatments have been assigned
@@ -596,24 +605,24 @@ shinyApp(
         tx <- unique(hot_to_r(input$metadata_hot)$treatment)
         updatePickerInput(session = session, inputId = "treatmentPickerTimeseries", choices = tx, selected = tx)
       }
-      
+
     }, ignoreInit = TRUE)
-    
+
     timeseriesDat <- reactive({
       anaDat <- alldata()
       selected_treatments <- input$treatmentPickerTimeseries
-      
+
       tsList <- lapply(selected_treatments, function(tx) {
         trt_dat <- subset(anaDat, treatment == tx)
         tsdat <- ana_to_timeseries(ana_df = trt_dat, waveform_labels = waveform_labels)$data
         tsdat$treatment <- tx
         return(tsdat)
       })
-      
+
       tsDat <- do.call(rbind, tsList)
       return(tsDat)
     }) %>% bindEvent(input$plotTimeseries)
-    
+
     # Timeseries Plot (for Display)
     tsPlot <- function() {
       modeDat <- timeseriesDat()
@@ -622,18 +631,18 @@ shinyApp(
                                  levels = toupper(intersect(names(waveform_labels), unique(modeDat$activity))))
       # remove 'end' for plotting
       modeDat <- subset(modeDat, modeDat$activity != 'END')
-      
+
       ggplot(modeDat, aes(x=activity, y=time, color=activity)) +
         geom_point(shape=20, size=1) +
         theme_classic(base_size = 16) +
         coord_flip() +
         facet_wrap(~treatment, ncol=1)
     }
-    
+
     output$timeseriesPlot <- renderPlot({
       tsPlot()
     })
-    
+
     # Export Timeseries Data
     output$downloadTimeseriesData <- downloadHandler(
       filename = function() {
@@ -643,7 +652,7 @@ shinyApp(
         write.csv(timeseriesDat(), file, row.names = FALSE)
       }
     )
-    
+
     # Export Timeseries Plot (SVG)
     output$downloadTimeseriesPlot <- downloadHandler(
       filename = function() {
@@ -655,10 +664,10 @@ shinyApp(
         dev.off()
       }
     )
-    
-    
+
+
     ### ----- Tab 5: Kinetograms   ----
-    
+
     # Update treatments available in pick-list
     observeEvent(input$tabsets, {
       # Run only if Kinetogram Tab/Page is selected, and treatments have been assigned
@@ -666,9 +675,9 @@ shinyApp(
         tx <- unique(hot_to_r(input$metadata_hot)$treatment)
         updatePickerInput(session = session, inputId = "treatmentPickerKinetogram", choices = tx, selected = tx)
       }
-      
+
     }, ignoreInit = TRUE)
-    
+
     # A list of 2 graph viz objects (one kinetogram "viz" per treatment)
     # kinetogramList <- reactive({
     #   deList <- dataAndErrorsList()
@@ -680,7 +689,7 @@ shinyApp(
     #   kinePlots <- compare_kinetograms(kineDat = kineDat)
     #   return(list(data = kineDat, plots = kinePlots))
     # }) %>% bindEvent(input$showKinetogram)
-    
+
     kinetogramList <- function() {
       deList <- dataAndErrorsList()
       anaDat <- deList$data
@@ -691,20 +700,20 @@ shinyApp(
       kinePlots <- compare_kinetograms(kineDat = kineDat)
       return(list(data = kineDat, plots = kinePlots))
     }
-    
+
     # kinetogramList <- reactive({
     #   kineList()
     # }) %>% bindEvent(input$showKinetogram)
-    
-    
+
+
     output$kinetogramPlot1 <- renderGrViz({
       kinetogramList()$plots[[1]]
     }) %>% bindEvent(input$showKinetogram)
-    
+
     output$kinetogramPlot2 <- renderGrViz({
       kinetogramList()$plots[[2]]
     }) %>% bindEvent(input$showKinetogram)
-    
+
     # Export Kinetogram Data
     output$downloadKinetogramData <- downloadHandler(
       filename = function() {
@@ -714,7 +723,7 @@ shinyApp(
         write.csv(kinetogramList()$data$summaryStats, file, row.names = FALSE)
       }
     )
-    
+
     # Export Kinetogram Plots (SVG)
     output$downloadKinetogramPlot1 <- downloadHandler(
       filename = function() {
@@ -738,8 +747,8 @@ shinyApp(
         close(fileConn)
       }
     )
-    
-    
+
+
   }
-  
+
 )
